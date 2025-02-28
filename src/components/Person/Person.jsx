6e782de4ee63ce07fccc3abc39492f728c79d98d
Roman Ohirok <matsuhita.2307@gmail.com>
2025-02-28 17:04:43 +0200

    add task slution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,23 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  let status = <p className="Person__partner">I am not married</p>;
+
+  if (person.isMarried) {
+    if (person.sex === 'm') {
+      status = (
+        <p className="Person__partner">{person.partnerName} is my wife</p>
+      );
+    } else {
+      status = (
+        <p className="Person__partner">{person.partnerName} is my husband</p>
+      );
+    }
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+      {person.age ? <p className="Person__age">I am {person.age}</p> : ''}
+      {status}
+    </section>
+  );
+};
