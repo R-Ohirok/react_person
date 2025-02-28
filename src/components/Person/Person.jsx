0eb0638e66f9f0fa47738851c6d3e11a5b50a1ec
Roman Ohirok <matsuhita.2307@gmail.com>
2025-02-28 17:22:38 +0200

    value destructed

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,22 +1,19 @@
 export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
   let status = <p className="Person__partner">I am not married</p>;
 
-  if (person.isMarried) {
-    if (person.sex === 'm') {
-      status = (
-        <p className="Person__partner">{person.partnerName} is my wife</p>
-      );
+  if (isMarried) {
+    if (sex === 'm') {
+      status = <p className="Person__partner">{partnerName} is my wife</p>;
     } else {
-      status = (
-        <p className="Person__partner">{person.partnerName} is my husband</p>
-      );
+      status = <p className="Person__partner">{partnerName} is my husband</p>;
     }
   }
 
   return (
     <section className="Person">
-      <h2 className="Person__name">My name is {person.name}</h2>
-      {person.age ? <p className="Person__age">I am {person.age}</p> : ''}
+      <h2 className="Person__name">My name is {name}</h2>
+      {age ? <p className="Person__age">I am {age}</p> : ''}
       {status}
     </section>
   );
